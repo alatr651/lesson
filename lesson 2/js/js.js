09e5c3209d9f1f28705fcc4a9e9835af524456bb
Dmitriy <win.7777@email.ua>
2018-03-28 00:41:50 +0300

    Простое задание на второй урок

--- lesson 2/js/js.js
+++ lesson 2/js/js.js
@@ -2,20 +2,95 @@ var budget = prompt("Ваш бюджет?", "");
 var shopName = prompt("Название Вашего магазина?", "");
 var open = true 
 var shopGoods = [];
-shopGoods [0] = prompt ("Какой тип товаров будем продавать?", "");
-shopGoods [1] = prompt ("Какой тип товаров будем продавать?", "");
-shopGoods [2] = prompt ("Какой тип товаров будем продавать?", "");
+
+
+
+
+
+
 
 mainList = {
 	budget,
 	shopName,
 	shopGoods ,
-	employers: {
-		name: "Ivan"
-	},
+	employers: {},
 	open
 }
 
+for (let i = 0; i < 5; i++) {
+
+	let a = prompt ("Какой тип товаров будем продавать?", "");
+	
+
+		if ((typeof(a)) === 'string' && (typeof(a)) !== null && a != '' && a.length < 50 ) {
+			console.log('все отлично');
+			mainList.shopGoods[i] = a;
+		} else {
+
+			if (a == '') {
+				let b = confirm("Вы точно желаете оставить это поле пустым?");
+				if (b) {
+					mainList.shopGoods[i] = '';
+				} else {
+					i--;
+					alert("Введите еще раз товар, который вы хотите продавать");
+				}
+			}
+		}
+}
+
+
+/*var i = 0;
+
+while (i < 5) {
+	let a = prompt ("Какой тип товаров будем продавать?", "");
+	i++
+
+		if ((typeof(a)) === 'string' && (typeof(a)) !== null && a != '' && a.length < 50 ) {
+			console.log('все отлично');
+			mainList.shopGoods[i] = a;
+		} else {
+
+			if (a == '') {
+				let b = confirm("Вы точно желаете оставить это поле пустым?");
+				if (b) {
+					mainList.shopGoods[i] = '';
+				} else {
+					i--;
+					alert("Введите еще раз товар, который вы хотите продавать");
+				}
+			}
+		}
+}*/
+
+/*	var i = 0;
+do {
+	let a = prompt ("Какой тип товаров будем продавать?", "");
+	i++
+
+		if ((typeof(a)) === 'string' && (typeof(a)) !== null && a != '' && a.length < 50 ) {
+			console.log('все отлично');
+			mainList.shopGoods[i] = a;
+		} else {
+
+			if (a == '') {
+				let b = confirm("Вы точно желаете оставить это поле пустым?");
+				if (b) {
+					mainList.shopGoods[i] = '';
+				} else {
+					i--;
+					alert("Введите еще раз товар, который вы хотите продавать");
+				}
+			}
+		}
+} 
+while (i < 5);*/
+
+
+
+
+
+
 console.log(mainList);
 
-alert("Ваш бюджет на один день состовляет " + budget);
+alert("Ваш бюджет на один день состовляет " + budget / 30);
